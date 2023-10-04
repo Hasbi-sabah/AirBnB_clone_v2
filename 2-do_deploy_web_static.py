66ca8f0efec9fd779c8443cab9e584a58cc780b4
H. Sabah <sabahhasbi00@gmail.com>
2023-10-04 22:42:01 +0000

    Code fix: try except added

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,6 @@
 """ Fabric script that generates a .tgz archive """
 from fabric.decorators import task
 from fabric.api import *
-import os
 
 env.hosts = ["100.27.4.102", "54.165.197.71"]
 
@@ -10,30 +9,34 @@ env.hosts = ["100.27.4.102", "54.165.197.71"]
 @task
 def do_deploy(archive_path):
     """Fabric script that distributes an archive to web servers"""
-    if not os.path.exists(archive_path):
+    try:
+        with_ext = archive_path.split("/")[-1]
+        without_ext = archive_path.split("/")[-1].split(".")[0]
+        put(archive_path, "/tmp")
+        run("mkdir -p /data/web_static/releases/" + without_ext)
+        run(
+            "tar -xzf /tmp/"
+            + with_ext + " -C /data/web_static/releases/"
+            + without_ext
+        )
+        run("rm /tmp/" + with_ext)
+        run(
+            "mv /data/web_static/releases/"
+            + without_ext
+            + "/web_static/* /data/web_static/releases/"
+            + without_ext
+        )
+        run("rm -rf /data/web_static/releases/"
+            + without_ext + "/web_static")
+        run("rm -rf /data/web_static/current")
+        run(
+            "ln -s /data/web_static/releases/"
+            + without_ext
+            + "/ /data/web_static/current"
+        )
+        return True
+    except Exception:
         return False
-    with_ext = archive_path.split("/")[-1]
-    without_ext = archive_path.split("/")[-1].split(".")[0]
-    put(archive_path, "/tmp")
-    run("mkdir -p /data/web_static/releases/" + without_ext)
-    run(
-        "tar -xzf /tmp/"
-        + with_ext
-        + " -C /data/web_static/releases/"
-        + without_ext
-    )
-    run("rm /tmp/" + with_ext)
-    run(
-        "mv /data/web_static/releases/"
-        + without_ext
-        + "/web_static/* /data/web_static/releases/"
-        + without_ext
-    )
-    run("rm -rf /data/web_static/releases/" + without_ext + "/web_static")
-    run("rm -rf /data/web_static/current")
-    run("ln -s /data/web_static/releases/"
-        + without_ext + "/ /data/web_static/current")
-    return True
 
 
 @task
