f0687e1cbaa10e0ec399391b41cca39f0091472b
H. Sabah <sabahhasbi00@gmail.com>
2023-10-04 20:17:32 +0000

    Code fix: made executable, fixed pycodestyle, added doc

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,4 +6,6 @@ from fabric.api import local
 
 @task
 def do_pack():
-    local("mkdir versions ; tar -cvzf versions/web_static_$(date +%Y%m%d%H%M%S).tgz web_static/")
+    """ generates a .tgz archive from web_static """
+    local("mkdir versions ; tar -cvzf \
+versions/web_static_$(date +%Y%m%d%H%M%S).tgz web_static/")
