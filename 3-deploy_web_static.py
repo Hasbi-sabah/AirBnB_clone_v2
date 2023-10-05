99f023d6ce25c575ee4c2bdc9457f33f09918a31
H. Sabah <sabahhasbi00@gmail.com>
2023-10-06 00:54:20 +0100

    Cleaning everything

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -12,39 +12,39 @@ env.hosts = ["100.27.4.102", "54.165.197.71"]
 def deploy():
     """ Fabric script that creates and distributes an archive """
     file_name = do_pack()
-    if not os.path.isfile('versions/{}'.format(file_name)):
+    if file_name is None:
         return False
-    return do_deploy('versions/{}'.format(file_name))
+    return do_deploy(file_name)
 
 
+@task
 def do_deploy(archive_path):
     """Fabric script that distributes an archive to web servers"""
-    if not os.path.isfile(archive_path):
+    try:
+        if not os.path.exists(archive_path):
+            return False
+        with_ext = archive_path.split("/")[-1]
+        without_ext = archive_path.split("/")[-1].split(".")[0]
+        pth = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("mkdir -p " + pth + without_ext)
+        run("tar -xzf /tmp/{} -C {}{}/".format(with_ext, pth, without_ext))
+        run("rm /tmp/{}".format(with_ext))
+        run("mv {1}{0}/web_static/* {1}{0}/".format(without_ext, pth))
+        run("rm -rf {}{}/web_static".format(pth, without_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(pth, without_ext))
+        return True
+    except Exception:
         return False
-    with_ext = archive_path.split("/")[-1]
-    without_ext = archive_path.split("/")[-1].split(".")[0]
-    put(archive_path, "/tmp")
-    run("mkdir -p /data/web_static/releases/{}".format(without_ext))
-    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}\
-".format(with_ext, without_ext))
-    run("rm /tmp/{}".format(with_ext))
-    run("mv /data/web_static/releases/{}/web_static/* \
-/data/web_static/releases/{}".format(without_ext, without_ext))
-    run("rm -rf /data/web_static/releases/{}/web_static\
-".format(without_ext))
-    run("rm -rf /data/web_static/current")
-    run("ln -s /data/web_static/releases/{}/ \
-/data/web_static/current".format(without_ext))
-    return True
 
 
 @runs_once
 def do_pack():
     """generates a .tgz archive from web_static"""
-    file_name = "web_static_{}.tgz\
-".format(datetime.now().strftime("%Y%m%d%H%M%S"))
-    local(
-        "mkdir versions ; tar -cvzf \
-versions/{} web_static/".format(file_name)
-    )
-    return file_name
+    datestr = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(datestr)
+    local("mkdir -p versions")
+    if local("tar -cvzf {} web_static/".format(file_name)).succeeded:
+        return file_name
+    return None
