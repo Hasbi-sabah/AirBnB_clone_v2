ddae108d68b1ce36f9d95fcbc45080ef6a9688b8
H. Sabah <sabahhasbi00@gmail.com>
2023-10-05 16:18:32 +0000

    Files added: task 4

---
+++ 100-clean_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Fabric script that generates a .tgz archive """
+from fabric.decorators import task
+from fabric.api import *
+from datetime import datetime
+import os
+
+env.hosts = ["100.27.4.102", "54.165.197.71"]
+
+
+@runs_once
+def clean_local(n):
+    """cleans the local versions only once"""
+    local("ls -dt ./versions/* | head -n -{} | xargs rm -fr".format(n))
+
+
+@task
+def do_clean(number=0):
+    """formats input and cleans remote"""
+    n = 1
+    if int(number) != 0:
+        n = int(number)
+    print(n)
+    run("ls -dt /data/web_static/releases/* | head -n \
+-{} | xargs rm -fr".format(n))
