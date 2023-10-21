fec0a09396c24db5150d85c5fbd1248f91bd8433
H. Sabah <sabahhasbi00@gmail.com>
2023-10-21 10:29:53 +0000

    Code fix: optimizing strict slashes, removing template dir

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,10 +5,11 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route("/states_list")
 def states_list():
     """ """
     states = storage.all(State)
